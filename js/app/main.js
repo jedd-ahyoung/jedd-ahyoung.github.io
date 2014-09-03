0f3a36ef8255f7fc6b5e550342468714c8ab8ad0
Jedd Ahyoung <jedd.ahyoung@gmail.com>
2014-09-03 17:52:23 -0400

    Style tweaks. Introduce a microjs lib for XHR and DOM.

--- js/app/main.js
+++
@@ -1,9 +0,0 @@
-angular.module('jedd.ahyoung', ['ngRoute'])
-	.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-		$locationProvider.html5Mode(true).hashPrefix('!');
-
-		$routeProvider
-			.when('/about', { templateUrl: 'partials/about.html' })
-			.when('/resume', { templateUrl: 'partials/resume.html' })
-			.otherwise({ redirectTo: '/' });
-	}]);
