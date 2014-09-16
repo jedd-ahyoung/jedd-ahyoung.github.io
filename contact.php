f1017c6b7e85bbd4cd082d1ac18c0430faa6fde2
Jedd Ahyoung <jedd.ahyoung@gmail.com>
2014-09-15 21:14:42 -0400

    Parenthesis hell.

--- contact.php
+++ contact.php
@@ -3,12 +3,12 @@
 /* Respond to the AJAX POST. */
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'
-	&& !empty($_POST['name']))
-	&& !empty($_POST['email']))
+	&& !empty($_POST['name'])
+	&& !empty($_POST['email'])
 	&& !empty($_POST['message']))
 {
 	// If antispam passes, parse the data and send via email.
-	if (empty($_POST['antispam']) {
+	if (empty($_POST['antispam'])) {
 		$to = 'jedd.ahyoung@gmail.com';
 		$from = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
 		$subject = str_replace(array("\r","\n"), array(" "," "), "Contact Form: Message from $_POST['name'] ($_POST['email']) ");
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'
 		$headers = 'From: ' . $from;
 	}
 
-	echo json_encode(array('success: ', mail($to, $subject, $message, $headers));
+	echo json_encode(array('success: ', mail($to, $subject, $message, $headers)));
 }
 
 ?>
